228bb1da3c58c5ac08aafb034fe8af9def9d3741
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2018-10-02 23:01:23 +0700

    Progress Login

--- login.php
+++ login.php
@@ -1,3 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+  </head>
+  <body>
+    <form action="prosesLogin.php" method="post">
+      <table>
+        <tr>
+          <td>Username</td>
+          <td><input type="text" name="username" placeholder="Username"></td>
+        </tr>
+        <tr>
+          <td>Password</td>
+          <td><input type="password" name="password" placeholder="Password"></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input type="submit" name="login" value="Login"></td>
+        </tr>
+      </table>
+    </form>
+  </body>
+</html>
 <?php
 
 
--- prosesLogin.php
+++ prosesLogin.php
@@ -1,7 +1,43 @@
-<?php 
+<?php
+session_start();
+$data = array(
+			array(
+				"user_id" => "10001",
+				"username" => "admin",
+				"password" => "admin",
+				"name" => "Ilham",
+				"level" => "admin"
+			),
+			array(
+				"user_id" => "20001",
+				"username" => "ilham",
+				"password" => "ilham",
+				"name" => "Fadhilah",
+				"level" => "user"
+			)
+		);
+if (isset($_POST['login'])) {
+  $username = isset($_POST['username']) ? $_POST['username'] : "";
+  $password = isset($_POST['password']) ? $_POST['password'] : "";
+  $a = 0;
+  $data_valid = 0;
+  while ($a < count($data)) {
+    if ($username == $data[$a]['username'] && $password == $data[$a]['password']) {
+      $_SESSION['user_id'] = $data[$a]['user_id'];
+      $_SESSION['username'] = $data[$a]['username'];
+      $_SESSION['name'] = $data[$a]['name'];
+      $_SESSION['level'] = $data[$a]['level'];
 
-
-
-
-
+      $data_valid = 1;
+      break;
+    }
+    $a++;
+  }
+  if ($data_valid > 0) {
+    header('location: registrasi.php');
+  }
+  else {
+    echo "Username atau Password Salah";
+  }
+}
 ?>
